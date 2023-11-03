c4b8c455316f85d2f5c4b1fc9a81dac2b98e66fe
MaybeDH <hasselc081@gmail.com>
2023-11-02 23:18:32 -0400

    Agregando archivo corregido de Titulo

---
+++ ProjectoFinal/ProjectoFinal/Context/AplicacionContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectoFinal.Models;
+
+namespace ProjectoFinal.Context
+{
+    public class AplicacionContext: DbContext
+    {
+        public AplicacionContext(DbContextOptions<AplicacionContext> options)
+     : base(options) { }
+        public DbSet<Titulo> Titulo { get; set; }
+    }
+}
